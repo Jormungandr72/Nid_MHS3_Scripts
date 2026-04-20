55891012554a482bc991047e0613c767f699ac91
Jormungandr72 <patrickjmcgranahan@gmail.com>
2026-04-20 14:24:00 -0500

    Updated genericget and added cache field.

--- reframework/autorun/data_modules/mhs3_genericget.lua
+++ reframework/autorun/data_modules/mhs3_genericget.lua
@@ -1,19 +1,19 @@
 -- MHS3 generic fetch module v1.0 by NidhoggX
--- Implemented: fetch_field()
--- TODO: fetch_method()
+-- Implemented: generic_field()
+-- TODO: generic_method(), clear_cache()
 
 -- ==========================================
 -- Establish as module
 -- ==========================================
 
-if genericfetch then return end -- Avoid loading module twice
-genericfetch = {} -- Stores module functions
+if genericget then return end -- Avoid loading module twice
+genericget = {} -- Stores module functions
 
 -- ==========================================
 -- Create logging functions
 -- ==========================================
 
-local MOD_NAME = "[MHS3 GENERICFETCH MODULE]"
+local MOD_NAME = "[MHS3 GENERICGET MODULE]"
 local function log_info(msg) log.info(MOD_NAME .. " " .. msg) end
 local function log_error(msg) log.error(MOD_NAME .. " " .. msg) end
 
@@ -21,8 +21,8 @@ local function log_error(msg) log.error(MOD_NAME .. " " .. msg) end
 -- Variables and config
 -- ==========================================
 
-local hooks_installed = false
-
+genericget.cache = {} -- Map of strings to methods/fields
+local hooks_installed = {} -- Map of hooked methods to booleans
 local example_data = {
     "app.SaveDataManager", -- Singleton
     "_UserSaveData", -- Every field on the path to the target field
@@ -33,7 +33,7 @@ local example_data = {
 -- Variables and config
 -- ==========================================
 
-function genericfetch.fetch_field(data_table)
+function genericget.generic_field(data_table)
     -- Gets a specified field from the game. 
     -- See the example_data table in this file for reference on formatting.
     -- Arguments:
@@ -45,10 +45,10 @@ function genericfetch.fetch_field(data_table)
     -- Get the singleton
     local ok_single, single = pcall(sdk.get_managed_singleton, data_table[1]) -- lua tables start at index 1
     if not ok_single or not single then
-        log_error("[fetch_field()] Unable to retrieve the following singleton: " .. data_table[1])
+        log_error("[generic_field()] Unable to retrieve the following singleton: " .. data_table[1])
         return nil
     else
-        log_info("[fetch_field()] Retrieved the following singleton: " .. data_table[1])
+        log_info("[generic_field()] Retrieved the following singleton: " .. data_table[1])
     end
 
     -- Loop through table until hitting target
@@ -57,14 +57,22 @@ function genericfetch.fetch_field(data_table)
     while data_table[i] do
         ok, ret_field = pcall(ret_field.get_field, ret_field, data_table[i])
         if not ok or not ret_field then
-            log_error("[fetch_field()] Unable to retrieve the following field: " .. data_table[i])
+            log_error("[generic_field()] Unable to retrieve the following field: " .. data_table[i])
             return nil
         else
-            log_info("[fetch_field()] Retrieved the following field: " .. data_table[i])
+            log_info("[generic_field()] Retrieved the following field: " .. data_table[i])
         end
         i = i + 1
     end
 
-    -- Return the target field
+    -- Reset index to nth element
+    i = i - 1
+    -- Cache and return the target field
+    genericget.cache[data_table[i]] = ret_field -- Last item in data table
+    log_info("[generic_field()] Cached the following field: " .. data_table[i])
+    log_info("[generic_field()] Returning the following field: " .. data_table[i])
     return ret_field
 end
+
+-- generic_method()
+-- clear_cache()
